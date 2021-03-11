7c9cc71eccdbf44f6eb3d18ddfcf84874ba96619
Hiroto710281 <65151692+Hiroto710281@users.noreply.github.com>
2021-03-11 11:09:37 +0900

    First commit

---
+++ いいね！/js/main.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  // Components
+
+  var likeComponent = Vue.extend({
+    props: ['message'],
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    template: '<button @click="countUp">Like {{count}}</button>',
+    methods: {
+      countUp: function() {
+        this.count++;
+
+      }
+    }
+  });
+
+  var app = new Vue({
+    el: '#app',
+    components: {
+      'like-component': likeComponent
+    }
+  });
+
+})();
